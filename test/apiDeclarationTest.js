61ee62ccbed9bc32b34e91ea98a99482fb378262
Timo Behrmann <timo.behrmann@gmail.com>
2014-08-01 10:24:27 +0200

    test

--- test/apiDeclarationTest.js
+++ test/apiDeclarationTest.js
@@ -17,7 +17,7 @@ describe('apiDeclaration', function () {
     Lab.beforeEach(function (done) {
         filterRoutesByTags = sinon.spy(utils, 'filterRoutesByTags');
         filterRoutesByPrefix = sinon.spy(utils, 'filterRoutesByPrefix');
-        groupRoutesByPath = sinon.stub(utils, 'groupRoutesByPath');
+        groupRoutesByPath = sinon.spy(utils, 'groupRoutesByPath');
         done();
     });
 
@@ -57,31 +57,162 @@ describe('apiDeclaration', function () {
     it('only routes: no params, no query, no payload, no response', function (done) {
         var settings = { mySettings: true };
         var apiDeclarator = apiDeclaration(settings);
-        var routingTable = [],
-            apiKey = 'Test',
-            models,
-            tags;
+        var routes = [],
+            apiKey = 'dev',
+            models = {},
+            tags = null;
 
-        groupRoutesByPath.returns({
-            '/': [
-                { path: '/', method: 'get' }
-            ],
-            '/dev': [
-                { path: '/dev', method: 'post'},
-                { path: '/dev', method: 'get'}
-            ],
-            '/dev/null': [
-                { path: '/dev/null', method: 'get' }
-            ]
-        });
+        var routes = [
+            { path: '/', method: 'get' },
+            { path: '/dev', method: 'post'},
+            { path: '/dev', method: 'get'},
+            { path: '/dev/null', method: 'get' }
+        ];
 
-        var list = apiDeclarator(routingTable, apiKey, models, tags);
+        var list = apiDeclarator(routes, apiKey, models, tags);
 
         expect(filterRoutesByTags.callCount).to.be.eql(1);
+        expect(filterRoutesByTags.calledWithExactly(settings, tags, routes)).to.be.ok;
         expect(filterRoutesByPrefix.callCount).to.be.eql(1);
-        expect(list).to.have.length(3);
+        expect(filterRoutesByPrefix.calledWithExactly(routes, apiKey)).to.be.ok;
+        expect(groupRoutesByPath.callCount).to.be.eql(1);
 
-        //TODO: joi validate!
+        var filteredRoutes = [
+            { path: '/dev', method: 'post' },
+            { path: '/dev', method: 'get' },
+            { path: '/dev/null', method: 'get' }
+        ];
+
+        expect(groupRoutesByPath.calledWithExactly(filteredRoutes)).to.be.ok;
+
+        expect(list).to.be.eql([
+            { path: "/dev", operations: [
+                { method: "GET", nickname: "get_dev", parameters: [], type: "void" },
+                { method: "POST", nickname: "post_dev", parameters: [], type: "void" }
+            ]},
+            { path: "/dev/null", operations: [
+                { method: "GET", nickname: "get_dev_null", parameters: [], type: "void" }
+            ]}
+        ]);
+
+        expect(list).to.have.length(2);
+
+        done();
+    });
+
+    it('deprecation tag', function (done) {
+        var settings = { mySettings: true };
+        var apiDeclarator = apiDeclaration(settings);
+        var list = apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['deprecated'] }},
+            { path: '/dev/normal', method: 'post' }
+        ], 'dev', null, null);
+        expect(list).to.have.length(2);
+        expect(list[0].operations).to.have.length(1).and.have.deep.property('[0].deprecated', true);
+        expect(list[1].operations).to.have.length(1).and.not.have.deep.property('[0].deprecated');
+        done();
+    });
+
+    it('api description', function (done) {
+        expect(apiDeclaration({})([
+            { path: '/dev', method: 'post' }
+        ], 'dev', null, null))
+            .to.have.length(1).and.not.to.have.deep.property('[0].description');
+
+        expect(apiDeclaration({ descriptions: { dev: 'test' }})([
+            { path: '/dev', method: 'post' }
+        ], 'dev', null, null))
+            .to.have.length(1).and.to.have.deep.property('[0].description').that.to.eql('test');
+
+        expect(apiDeclaration({ descriptions: { dev: 'test' }})([
+            { path: '/dev', method: 'post' }
+        ], 'dev', null, null, { descriptions: { dev: 'test2'}}))
+            .to.have.length(1).and.to.have.deep.property('[0].description').that.to.eql('test2');
+
+        done();
+    });
+
+    it('operation grouping', function (done) {
+        var settings = { mySettings: true };
+        var apiDeclarator = apiDeclaration(settings);
+        var list = apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['deprecated'] }},
+            { path: '/dev', method: 'get' }
+        ], 'dev', null, null);
+        expect(list).to.have.length(1).and.have.deep.property('[0].operations').that.have.length(2);
+        done();
+    });
+
+    it('tag filtering', function (done) {
+        var settings = { mySettings: true };
+        var apiDeclarator = apiDeclaration(settings);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get' }
+        ], 'dev', null, 'dev'))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, ['dev']))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, ['dev', 'api']))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(2);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, 'dev,api'))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(2);
+
+        done();
+    });
+
+    it('requiredTag', function (done) {
+        var settings = { requiredTag: 'api' };
+        var apiDeclarator = apiDeclaration(settings);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get' }
+        ], 'dev', null, 'dev'))
+            .to.have.length(0);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev', 'api'] }},
+            { path: '/dev', method: 'get' }
+        ], 'dev', null, 'dev'))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, ['dev']))
+            .to.have.length(0);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev', 'api'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, ['dev']))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, ['dev', 'api']))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
+
+        expect(apiDeclarator([
+            { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+            { path: '/dev', method: 'get', settings: { tags: ['api'] } }
+        ], 'dev', null, 'dev,api'))
+            .to.have.length(1).and.have.deep.property('[0].operations').that.have.length(1);
 
         done();
     });
--- test/apiListingTest.js
+++ test/apiListingTest.js
@@ -16,48 +16,114 @@ var sinon = require('sinon');
 describe('apiListing', function () {
     var filterRoutesByTags, extractAPIKeys, getDescription;
 
-    Lab.beforeEach(function (done) {
-        filterRoutesByTags = sinon.stub(utils, 'filterRoutesByTags');
-        extractAPIKeys = sinon.stub(utils, 'extractAPIKeys');
-        getDescription = sinon.stub(utils, 'getDescription');
-        done();
+    describe('integration', function() {
+        it('#1', function (done) {
+            var routes = [
+                { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+                { path: '/test', method: 'get', settings: { tags: ['dev', 'api'] }}
+            ];
+            var settings = {};
+            var swaggerServerSettings = {};
+            var tags = null;
+            var list = apiListing(settings)(routes, tags, swaggerServerSettings);
+            expect(list).to.exist.and.to.have.length(2).and.to.have.deep.property('[0].path').that.to.eql('/dev');
+            done();
+        });
+
+        it('#2', function (done) {
+            var routes = [
+                { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+                { path: '/test', method: 'get', settings: { tags: ['dev', 'api'] }}
+            ];
+            var settings = { requiredTag: 'api' };
+            var swaggerServerSettings = {};
+            var tags = null;
+            var list = apiListing(settings)(routes, tags, swaggerServerSettings);
+            expect(list).to.exist.and.to.have.length(1).and.to.have.deep.property('[0].path').that.to.eql('/test');
+            done();
+        });
+
+        it('#3', function (done) {
+            var routes = [
+                { path: '/dev', method: 'post', settings: { tags: ['dev'] }},
+                { path: '/test', method: 'get', settings: { tags: ['dev', 'api'] }},
+                { path: '/zong', method: 'get', settings: { tags: ['dev', 'api', 'test'] }}
+            ];
+            var settings = { requiredTag: 'api' };
+            var swaggerServerSettings = {};
+
+            expect(apiListing(settings)(routes, ['test'], swaggerServerSettings))
+                .to.exist.and.to.have.length(1).and.to.have.deep.property('[0].path').that.to.eql('/zong');
+            expect(apiListing(settings)(routes, 'test', swaggerServerSettings))
+                .to.exist.and.to.have.length(1).and.to.have.deep.property('[0].path').that.to.eql('/zong');
+            expect(apiListing(settings)(routes, 'api', swaggerServerSettings))
+                .to.exist.and.to.have.length(2);
+
+            done();
+        });
+
+        it('description', function (done) {
+            var routes = [
+                { path: '/test', method: 'get', settings: { tags: ['api'] }}
+            ];
+
+            var settings = {
+                requiredTag: 'api',
+                descriptions: { test: 'mep' }
+            };
+
+            expect(apiListing(settings)(routes, null, {}))
+                .to.exist.and.to.have.length(1)
+                .and.to.have.deep.property('[0]').that.to.eql({ path: '/test', description: 'mep' });
+
+            expect(apiListing(settings)(routes, null, { descriptions: { test: 'mep2' }}))
+                .to.exist.and.to.have.length(1)
+                .and.to.have.deep.property('[0]').that.to.eql({ path: '/test', description: 'mep2' });
+
+            done();
+        });
     });
 
-    Lab.afterEach(function (done) {
-        utils.filterRoutesByTags.restore();
-        utils.extractAPIKeys.restore();
-        utils.getDescription.restore();
-        done();
-    });
+    describe('flow', function () {
+        Lab.beforeEach(function (done) {
+            filterRoutesByTags = sinon.stub(utils, 'filterRoutesByTags');
+            extractAPIKeys = sinon.stub(utils, 'extractAPIKeys');
+            getDescription = sinon.stub(utils, 'getDescription');
+            done();
+        });
 
-    it('empty', function (done) {
-        var apiLister = apiListing({});
-        var list = apiLister([], {});
-        expect(list).to.exist;
-        expect(list).to.be.empty;
-        done();
-    });
+        Lab.afterEach(function (done) {
+            utils.filterRoutesByTags.restore();
+            utils.extractAPIKeys.restore();
+            utils.getDescription.restore();
+            done();
+        });
 
-    it('filterRoutesByTags', function (done) {
-        var settings = {};
-        var routeList = [
-            { path: '/dev/null', method: 'get', settings: { tags: ['Hapi'] } },
-            { path: '/dev/null', method: 'get', settings: { tags: ['api', 'Hapi'] } }
-        ];
+        it('empty', function (done) {
+            var apiLister = apiListing({});
+            var list = apiLister([], {});
+            expect(list).to.exist;
+            expect(list).to.be.empty;
+            done();
+        });
 
-        var apiKeys = [ 'hapi' ];
-        filterRoutesByTags.returns(routeList);
-        extractAPIKeys.withArgs(routeList).returns(apiKeys);
-        getDescription.withArgs(settings, apiKeys[0]).returns('MyTestDescription');
+        it('filterRoutesByTags', function (done) {
+            var settings = {};
+            var routeList = [
+                { path: '/dev/null', method: 'get', settings: { tags: ['Hapi'] } },
+                { path: '/dev/null', method: 'get', settings: { tags: ['api', 'Hapi'] } }
+            ];
 
-        var apiLister = apiListing(settings);
-        var list = apiLister([], {});
+            var apiKeys = [ 'hapi' ];
+            filterRoutesByTags.returns(routeList);
+            extractAPIKeys.withArgs(routeList).returns(apiKeys);
+            getDescription.withArgs(settings, apiKeys[0]).returns('MyTestDescription');
 
-        expect(list).to.exist;
-        expect(list).to.be.eql([ { path: 'hapi', description: 'MyTestDescription' } ]);
-
-        //TODO: joi validate!
-
-        done();
+            var apiLister = apiListing(settings);
+            expect(apiLister([], {})).to.exist.and.to.be.eql([
+                { path: 'hapi', description: 'MyTestDescription' }
+            ]);
+            done();
+        });
     });
 });
